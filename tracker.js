20d5cd39128af376ef3df5475d494aecf13c67ff
dhirajfromindia <53819733+dhirajfromindia@users.noreply.github.com>
2026-05-01 16:21:04 +0530

    Fetch and log data from stock API

--- tracker.js
+++ tracker.js
@@ -1,1 +1,9 @@
-console.log("tracker running");
+const axios = require("axios");
+
+async function run(){
+ const res = await axios.get("https://api.stockedge.com/Api/SecurityDashboardApi/GetComposedIndexParts/14801");
+ console.log("Total Companies:", res.data.length);
+ console.log(res.data[0]);
+}
+
+run();
